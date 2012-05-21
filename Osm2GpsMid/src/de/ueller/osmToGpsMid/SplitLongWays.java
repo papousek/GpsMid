a4a82fa8352a692bd859581b62e965c966c8bba7
papousek <jan.papousek@gmail.com>
2012-05-21 18:19:27 +0200

    removed logging to err output

--- Osm2GpsMid/src/de/ueller/osmToGpsMid/SplitLongWays.java
+++ Osm2GpsMid/src/de/ueller/osmToGpsMid/SplitLongWays.java
@@ -1,12 +1,12 @@
 /**
- * This file is part of OSM2GpsMid 
+ * This file is part of OSM2GpsMid
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as published by
  * the Free Software Foundation.
  *
  * Copyright (C) 2007  Harald Mueller
- * 
+ *
  */
 package de.ueller.osmToGpsMid;
 
@@ -19,8 +19,8 @@ import de.ueller.osmToGpsMid.model.Way;
 public class SplitLongWays {
 	OsmParser parser;
 	LinkedList<Way> added=new LinkedList<Way>();
-	
-	
+
+
 	public SplitLongWays(OsmParser parser) {
 		super();
 		int count = 0;
@@ -28,12 +28,12 @@ public class SplitLongWays {
 		for (Way way : parser.getWays()) {
 			testAndSplit(way);
 			count++;
-			if (count % 500 == 0) {
-				System.err.println("Tested " + count 
-						   + " ways for splitting");
-			}
+//			if (count % 500 == 0) {
+//				System.err.println("Tested " + count
+//						   + " ways for splitting");
+//			}
 		}
-		for (Way w : added) {			
+		for (Way w : added) {
 			parser.addWay(w);
 		}
 		count = 0;
@@ -44,15 +44,15 @@ public class SplitLongWays {
 				}
 				if (way.triangles != null && way.triangles.size() > 0) {
 					count++;
-					if (count % 500 == 0) {
-						System.err.println("Did " + count 
-								   + " recreatePath()s");
-					}
+//					if (count % 500 == 0) {
+//						System.err.println("Did " + count
+//								   + " recreatePath()s");
+//					}
 					way.recreatePathAvoidDuplicates();
 				}
 			}
 
-		}		
+		}
 		added=null;
 	}
 
@@ -62,7 +62,7 @@ public class SplitLongWays {
 //		if ( way.isArea()) return;
 		// TODO: Length of one longitude degree gets smallen when aproaching the poles.
 		Bounds b=way.getBounds();
-		if ((b.maxLat-b.minLat) > 0.09f 
+		if ((b.maxLat-b.minLat) > 0.09f
 				|| (b.maxLon-b.minLon) > 0.09f ){
 			//System.err.println("Splitting " + (way.isArea() ? "area " :"way ") + way.toUrl());
 			Way newWay=way.split();
@@ -76,7 +76,7 @@ public class SplitLongWays {
 //				}
 				testAndSplit(way);
 				testAndSplit(newWay);
-			} 
+			}
 		}
 	}
 }
